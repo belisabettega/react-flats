56864d0259eb862b0299ee7e01f6e4b0dea49e97
belisa bettega <belisabettega@gmail.com>
2021-09-28 13:51:02 -0300

    feat: create containers

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -5,38 +5,42 @@ import flats from '../../data/flats.js'
 import Marker from './marker'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = {
-      selectedFlat: flats[0],
-      flats
-    };
-  }
-  center(){
-    return {
-      lat: this.state.selectedFlat.lat,
-      lng: this.state.selectedFlat.lng
-    };
-  }
+  //   this.state = {
+  //     selectedFlat: flats[0],
+  //     flats
+  //   };
+  // }
+  // center(){
+  //   return {
+  //     lat: this.state.selectedFlat.lat,
+  //     lng: this.state.selectedFlat.lng
+  //   };
+  // }
 
-  selectFlat = (index) => {
-    this.setState({ selectedFlat: flats[index] });
-  }
+  // selectFlat = (index) => {
+  //   this.setState({ selectedFlat: flats[index] });
+  // }
   
   render(){
     return(
+    // <div>
+    //   < FlatList 
+    //     flats={flats}
+    //     selectedFlat={this.state.selectedFlat}
+    //     selectFlat={this.selectFlat}
+    //   />
+    //   <div className="map-container">
+    //     <GoogleMapReact defaultCenter={this.center()} defaultZoom={12}>
+    //       <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng}/>
+    //     </GoogleMapReact>
+    //   </div>
+    // </div>
     <div>
-      < FlatList 
-        flats={flats}
-        selectedFlat={this.state.selectedFlat}
-        selectFlat={this.selectFlat}
-      />
-      <div className="map-container">
-        <GoogleMapReact defaultCenter={this.center()} defaultZoom={12}>
-          <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng}/>
-        </GoogleMapReact>
-      </div>
+      < FlatList />
+      < Map />
     </div>
     );
   }
